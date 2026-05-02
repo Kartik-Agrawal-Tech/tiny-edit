fda6cdfd627cde2314c8785c8812a8088c15c586
Kartik Agrawal <kartikagrawal530@gmail.com>
2026-05-02 09:44:43 +0530

    chore: gitignore lab/.work scratch dir

--- lab/.work/D-delete-arrow-function-line/utils.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-  return 'Hello, ' + name;
-}
-
-
-function sum(arr) {
-  return arr.reduce((a, b) => a + b, 0);
-}
--- lab/.work/MV-rename-file/helpers.js
+++
@@ -1,9 +0,0 @@
-function greet(name) {
-  return 'Hello, ' + name;
-}
-
-const double = (x) => x * 2;
-
-function sum(arr) {
-  return arr.reduce((a, b) => a + b, 0);
-}
